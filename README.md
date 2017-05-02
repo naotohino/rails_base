d585c010a210f938696d29c90adbb2e72c384ee0
Naoto Hino <naoto.hino@sonymobile.com>
2017-05-02 17:53:47 +0900

    Add devise and devise_token_auth

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # README
 
+Base Rails App created by
+rails new --database=mysql --skip-turbolinks --skip-test
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root 'home#index'
+
+  namespace :api, format: :json  do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    end
+    namespace :v1 do
+      #get '/users', controller: :users, action: :index
+    end
+  end
+
+  devise_for :users
+
 end
